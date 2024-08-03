b72fc3299b6c288ccf60cd4c7f8b5abe65e0b1a8
Diivvuu <divyanshubawa9@gmail.com>
2024-08-03 18:42:17 +0530

    mongo db connected and envVars created

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+
 import authRoutes from "./routes/auth.Routes.js";
+import { connectDB } from "./config/db.js";
+
 const app = express();
+
+// const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Server started at port 5000");
+  connectDB();
 });
