a9e52d1aadb951c34877bfa4da5d90732da88f52
Diivvuu <divyanshubawa9@gmail.com>
2024-08-05 02:47:20 +0530

    auth controllers, routing, middleware, check done

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,12 @@ import path from "path";
 
 import authRoutes from "./routes/auth.Routes.js";
 import { connectDB } from "./config/db.js";
+import { ENV_VARS } from "./config/envVars.js";
 
 const app = express();
 
-// const PORT = ENV_VARS.PORT;
-const __dirname = path.resolve;
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,6 +17,6 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 
 app.listen(5000, () => {
-  console.log("Server started at port 5000");
+  console.log("Server started at port http://localhost:" + PORT);
   connectDB();
 });
