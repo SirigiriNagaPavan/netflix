554df68a8f0b3d9f7e19b53235c5e48571838694
Diivvuu <divyanshubawa9@gmail.com>
2024-08-26 02:38:25 +0530

    auth routes

--- backend/routes/auth.Routes.js
+++ backend/routes/auth.Routes.js
@@ -1,17 +1,17 @@
-import express from "express";
+import express from 'express';
 import {
   authCheck,
   login,
   logout,
   signup,
-} from "../controllers/auth.controller.js";
-import { protectRoute } from "../middleware/protectRoute.js";
+} from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/logout", logout);
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/logout', logout);
 
-router.get("/authCheck", protectRoute, authCheck);
+router.get('/authCheck', protectRoute, authCheck);
 export default router;
